c2efb7b3e0942d60ab3eda30ce3865f004c3d5ef
f3xbox <155007127+f3xbox@users.noreply.github.com>
2024-06-03 08:57:47 -0400

    FNG picture

--- src/app/fng/page.tsx
+++ src/app/fng/page.tsx
@@ -5,7 +5,7 @@ import Footer from '../_components/Footer';
 import Hero from '../_components/Hero';
 
 /** replace with a regional image */
-import f3HeroImg from '../../../public/f3-refuge-2023-11-09.jpg';
+import f3HeroImg from '../../../public/FroYo.png';
 
 import en from '../../locales/en.json'
 
